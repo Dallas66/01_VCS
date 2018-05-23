71076548f86f13ba54ce2f2e182acb64043d7ee5
Dallas66 <protasov001@gmail.com>
2018-05-23 11:48:08 +0400

    another senior java developer commit try conflict

--- src/main/java/FirstLogicClass.java
+++ src/main/java/FirstLogicClass.java
@@ -3,5 +3,7 @@ public class FirstLogicClass {
     public static void logicHere(){
 
         System.out.println("Some logic");
+
+        System.out.println("Another logic try to conflict here or not");
     }
 }

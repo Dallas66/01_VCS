a1f66c2df253c831065731837accab73dc4a176f
Dallas66 <protasov001@gmail.com>
2018-05-23 12:01:07 +0400

    Git changes

--- src/main/java/FirstLogicClass.java
+++ src/main/java/FirstLogicClass.java
@@ -3,6 +3,8 @@ public class FirstLogicClass {
     public static void logicHere(){
 
         System.out.println("Some logic");
+        
+        System.out.println("text from git");
 
         System.out.println("Another logic");
     }
